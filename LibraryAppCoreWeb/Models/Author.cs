409e4f9e46c1f33b6c3b6da90381c315f2557fbe
Daniel Johansson <johansson.gd@gmail.com>
2019-06-10 21:04:12 +0200

    Lade till Book och Author-klasser till modellen.

---
+++ LibraryAppCoreWeb/Models/Author.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryAppCoreWeb.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+    }
+}
