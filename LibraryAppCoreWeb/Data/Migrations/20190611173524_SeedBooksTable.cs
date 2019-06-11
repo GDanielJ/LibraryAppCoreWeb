242ac19d3a2a3d726835f621ce1323725b8d8917
Daniel Johansson <johansson.gd@gmail.com>
2019-06-11 20:38:55 +0200

    Använder EF Core i BooksController nu.

---
+++ LibraryAppCoreWeb/Data/Migrations/20190611173524_SeedBooksTable.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibraryAppCoreWeb.Data.Migrations
+{
+    public partial class SeedBooksTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Books (Name, ReleaseDate) VALUES ('The Fellowship of the Ring', '1954-07-29 12:00:00')");
+            migrationBuilder.Sql("INSERT INTO Books (Name, ReleaseDate) VALUES ('A Game of Thrones', '1996-08-01 12:00:00')");
+            migrationBuilder.Sql("INSERT INTO Books (Name, ReleaseDate) VALUES ('The Hunger Games', '2008-09-14 12:00:00')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
