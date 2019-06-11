22915a07e9743d323c0ad728c6c3b9f17080fc0b
Daniel Johansson <johansson.gd@gmail.com>
2019-06-11 21:10:38 +0200

    Lagt till AuthorsController och view: Index.

---
+++ LibraryAppCoreWeb/Controllers/AuthorsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryAppCoreWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryAppCoreWeb.Controllers
+{
+    public class AuthorsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        // TODO - Seeda databasen med författare
+
+        public AuthorsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var authors = _context.Authors.ToList();
+
+            return View(authors);
+        }
+    }
+}
