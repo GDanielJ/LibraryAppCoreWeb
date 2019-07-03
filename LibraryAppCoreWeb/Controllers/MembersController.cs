066fe60bf2535927eb597c61b4304a42b427d884
Daniel Johansson <johansson.gd@gmail.com>
2019-07-03 19:43:43 +0200

    Skapat Member i modell, MembersController och Index (view) till MembersController.

---
+++ LibraryAppCoreWeb/Controllers/MembersController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LibraryAppCoreWeb.Data;
+
+namespace LibraryAppCoreWeb.Controllers
+{
+    public class MembersController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public MembersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var members = _context.Members.ToList();
+
+            return View(members);
+        }
+    }
+}
--- LibraryAppCoreWeb/Data/ApplicationDbContext.cs
+++ LibraryAppCoreWeb/Data/ApplicationDbContext.cs
@@ -15,7 +15,7 @@ namespace LibraryAppCoreWeb.Data
         }
 
         public DbSet<Book> Books { get; set; }
-
         public DbSet<Author> Authors { get; set; }
+        public DbSet<Member> Members { get; set; }
     }
 }
